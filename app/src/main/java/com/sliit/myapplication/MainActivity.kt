0ec88fbfb5170a0043e9f4c48a067c570e03b8c7
Rashika1998 <it18213140@my.sliit.lk>
2020-05-24 20:47:46 +0530

    Kotlin tutorial has been implemented.

---
+++ app/src/main/java/com/sliit/myapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.sliit.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.* import java.util.*
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(View.OnClickListener{
+            onSubmit(inputYear);
+        })
+    }
+
+    fun onSubmit(view:View) {
+
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        txtAge.text="Age :" + (currentYear - inputYear.text.toString().toInt())
+    }
+}
